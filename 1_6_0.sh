10972e5dd1015418938ecf0ab488f68105ab4929
Matthew Gharrity <gharrma@google.com>
2021-11-11 22:19:05 +0000

    Update script for 1.6.0-RC2

--- 1_6_0.sh
+++ 1_6_0.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env bash
 set -eu
 
-mkdir -p out/203-1.6.0-RC
-mkdir -p out/211-1.6.0-RC
+mkdir -p out/203-1.6.0-RC2
+mkdir -p out/211-1.6.0-RC2
+mkdir -p out/212-1.6.0-RC2
 
-./run_with_urls.sh out/203-1.6.0-RC \
+./run_with_urls.sh out/203-1.6.0-RC2 \
     'https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2020.3.1.25/android-studio-2020.3.1.25-linux.tar.gz' \
-    'https://plugins.jetbrains.com/plugin/download?rel=true&updateId=140918' 2>&1 > out/203-1.6.0-RC/log.txt &
+    'https://plugins.jetbrains.com/plugin/download?rel=true&updateId=144126' 2>&1 > out/203-1.6.0-RC2/log.txt &
 
-./run_with_urls.sh out/211-1.6.0-RC \
-    'https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2021.1.1.13/android-studio-2021.1.1.13-linux.tar.gz' \
-    'https://plugins.jetbrains.com/plugin/download?rel=true&updateId=140919' 2>&1 > out/211-1.6.0-RC/log.txt &
+./run_with_urls.sh out/211-1.6.0-RC2 \
+    'https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2021.1.1.16/android-studio-2021.1.1.16-linux.tar.gz' \
+    'https://plugins.jetbrains.com/plugin/download?rel=true&updateId=144127' 2>&1 > out/211-1.6.0-RC2/log.txt &
+
+./run_with_urls.sh out/212-1.6.0-RC2 \
+    'https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2021.2.1.4/android-studio-2021.2.1.4-linux.tar.gz' \
+    'https://plugins.jetbrains.com/plugin/download?rel=true&updateId=144128' 2>&1 > out/212-1.6.0-RC2/log.txt &
 
 wait $(jobs -p)
 
-cat out/{203,211}-1.6.0-RC/new-errors.txt
+cat out/{203,211,212}-1.6.0-RC2/new-errors.txt
