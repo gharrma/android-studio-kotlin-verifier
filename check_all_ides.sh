ba0a31647802f6661c769244c6e66ea1125c91b9
Matthew Gharrity <gharrma@google.com>
2022-03-27 19:37:54 +0000

    Kotlin 1.6.20-RC2

--- check_all_ides.sh
+++ check_all_ides.sh
@@ -10,17 +10,13 @@ function check_ide {
 
 echo "Scheduling verifications."
 
-check_ide 'TODO_IJ_VERSION' \
-    'TODO_STUDIO_LINK' \
-    'TODO_KOTLIN_LINK'
+check_ide '211' \
+    'https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2021.1.1.22/android-studio-2021.1.1.22-linux.tar.gz' \
+    'https://plugins.jetbrains.com/plugin/download?rel=true&updateId=163889'
 
-check_ide 'TODO_IJ_VERSION' \
-    'TODO_STUDIO_LINK' \
-    'TODO_KOTLIN_LINK'
-
-check_ide 'TODO_IJ_VERSION' \
-    'TODO_STUDIO_LINK' \
-    'TODO_KOTLIN_LINK'
+check_ide '212' \
+    'https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2021.2.1.11/android-studio-2021.2.1.11-linux.tar.gz' \
+    'https://plugins.jetbrains.com/plugin/download?rel=true&updateId=163890'
 
 echo "Waiting for verifications to complete."
 wait $(jobs -p)
